6e6bc62701673df220a686a47b2bceace40e02eb
JaimeVilches87 <jaimevilches87@gmail.com>
2021-06-21 23:44:51 -0400

    added code for engineer and intern

--- app.js
+++ app.js
@@ -53,6 +53,94 @@ const ManagerQuestions = () => {
         })
 };
 
+//first ask if they want to enter an intern, engineer or if they are finished
+const EmployeeQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Would you like to add an Engineer, an Intern or are you finished?',
+            choices: ['Engineer', 'Intern', 'I am finished']
+        }])
+        //if Engineer is selected then
+        .then(({ choice }) => {
+            if (choice === 'Engineer') {
+                return inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'Please Enter The Engineer\'s Name'
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: 'Please Enter The Engineer\'s Employee ID'
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'Please Enter The Engineer\'s Email Address'
+                    },
+                    {
+                        type: 'input',
+                        name: 'github',
+                        message: 'What is the Engineer\'s Github username?'
+                    }
+                ])
+
+                    //create a new object of engineer and push it to the array, 
+                    // then call employee questions again
+                    .then(({ name, id, email, github }) => {
+                        employeeArray.engineer.push(new Engineer(name, id, email, github))
+
+                        return EmployeeQuestions();
+                    })
+            }
+
+            //if Intern is selected then
+            else if (choice === 'Intern') {
+                return inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'Please Enter The Intern\'s Name'
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: 'Please Enter The Intern\'s Employee ID'
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: 'Please Enter The Intern\'s Email Address'
+                    },
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: 'Please enter Intern\'s school name'
+                    }
+                ])
+
+                    //create a new object of engineer and push it to the array, 
+                    // then call employee questions again
+                    .then(({ name, id, email, school }) => {
+                        employeeArray.intern.push(new Intern(name, id, email, school))
+
+                        return EmployeeQuestions();
+                    })
+            }
+            //when I am Finished is selected then call function to write html
+            else 
+            {
+                writeHTML();
+            }
+        })
+};
+
+// function to call render function to return HTML and write it on team.html
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
