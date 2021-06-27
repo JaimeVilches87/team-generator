c358a86c5f7824d8996e7f37f74036c10463068a
JaimeVilches87 <jaimevilches87@gmail.com>
2021-06-27 11:22:27 -0400

    updated code

--- app.js
+++ app.js
@@ -4,19 +4,16 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require('util');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputFile = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
 
 //Array containing employee info that will be used to push into render function
-const employeeArray = {
-    'manager': [],
-    'engineer': [],
-    'intern': []
-};
+const employeeArray = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -48,7 +45,7 @@ const ManagerQuestions = () => {
         //create a new object of the manager and push it to the array, 
         // then call employee questions
         .then(({ name, id, email, officeNumber }) => {
-            employeeArray.manager.push(new Manager(name, id, email, officeNumber))
+            employeeArray.push(new Manager(name, id, email, officeNumber))
             EmployeeQuestions();
         })
 };
@@ -91,7 +88,7 @@ const EmployeeQuestions = () => {
                     //create a new object of engineer and push it to the array, 
                     // then call employee questions again
                     .then(({ name, id, email, github }) => {
-                        employeeArray.engineer.push(new Engineer(name, id, email, github))
+                        employeeArray.push(new Engineer(name, id, email, github))
 
                         return EmployeeQuestions();
                     })
@@ -125,7 +122,7 @@ const EmployeeQuestions = () => {
                     //create a new object of engineer and push it to the array, 
                     // then call employee questions again
                     .then(({ name, id, email, school }) => {
-                        employeeArray.intern.push(new Intern(name, id, email, school))
+                        employeeArray.push(new Intern(name, id, email, school))
 
                         return EmployeeQuestions();
                     })
@@ -140,8 +137,10 @@ const EmployeeQuestions = () => {
 
 // function to call render function to return HTML and write it on team.html
 const writeHTML = () => {
+    console.log(employeeArray);
     const dataTemplate = render(employeeArray);
-    fs.writeFileSync('./output/team.html', dataTemplate);
+    fs.writeFileSync(outputFile, dataTemplate);
+
 }
 
 
@@ -158,22 +157,3 @@ const init = async () => {
 // Function call to initialize app
 init();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
